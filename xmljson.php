13723ae952436b8ac420fbd2b2b2a0672893be26
CRC <46287826+pysocrates@users.noreply.github.com>
2020-02-12 11:38:42 -0500

    Update xmljson.php

--- xmljson.php
+++ xmljson.php
@@ -1,8 +1,10 @@
 <?php
+
 //get XML
-$xml = simplexml_load_string($xml_string);
-// encode to json
+$xml = simplexml_load_string($xmlstring);
+// convert to json
 $json = json_encode($xml);
 // return array
 $array = json_decode($json,TRUE);
+
 ?>
